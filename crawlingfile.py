2664f5ebc3ebbb86682d964fa507b6f63759d3d4
Hyeongjun Kim <hyeongjunkim@Hyeongjunui-MacBookAir.local>
2020-02-15 14:21:53 +0900

    crawing upload by BeuatifulSoup4

---
+++ crawlingfile.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get('https://www.billboard.com/charts/hot-100')
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+rank = soup.select(
+   'li > button > span.chart-element__rank.flex--column.flex--xy-center.flex--no-shrink > span.chart-element__trend > i > span'
+)
+song = soup.select(
+   'li > button > span.chart-element__information > span.chart-element__information__song.text--truncate.color--primary'
+)
+singer = soup.select(
+   'li > button > span.chart-element__information > span.chart-element__information__artist.text--truncate.color--secondary'
+)
+music_chart = []
+for items in zip(rank, song, singer):
+    music_chart.append(
+        {
+            'rank'  : items[0].text,
+            'song'  : items[1].text,
+            'singer': items[2].text,
+        }
+    )
+
+for row in music_chart:
+    print(row['rank'],'|', row['song'],'|' ,row['singer'])
